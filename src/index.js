5f36caca2457af48134bfba77b060f8b794d6a77
anarandjic <ana@geta.no>
2023-11-15 11:01:10 +0100

    more logging

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const generate = async () => {
-    console.log('launching a new chrome instance')
+    console.log('launching a new chrome instance');
 
     const browser = await puppeteer.launch({
-        headless: 'new'
+        headless: 'new',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
-    console.log('creating new page')
+    console.log('creating new page');
     const page = await browser.newPage();
 
-    console.log('setting html as the pages content')
+    console.log('setting html as the pages content');
     const html = fs.readFileSync(`${__dirname}/template.html`, 'utf-8');
     await page.setContent(html, {
         waitUntil: 'domcontentloaded'
     });
 
-    console.log('creating .pdf file')
-    await page.pdf({ format: 'A4', path: `${__dirname}/pdf-template.pdf` });
+    console.log('creating .pdf file');
+    await page.pdf({ format: 'A4', path: `${__dirname}/result.pdf` });
 
+    console.log('.pdf file created');
+    console.log('closing browser');
     await browser.close()
 }
 
 generate();
-console.log('Done!');
